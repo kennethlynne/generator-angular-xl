36d8baa858126310d70445364b788b459849e757
Kenneth Lynne <kenneth.lynne@gmail.com>
2013-11-16 20:00:41 +0100

    Added missing dependancy

--- templates/common/root/app/scripts/components/sockless/sockless.js
+++ templates/common/root/app/scripts/components/sockless/sockless.js
@@ -1,13 +1,12 @@
 'use strict';
 
-angular.module('socklessJS.components')
+angular.module('socklessJS.components.sockless', ['socklessJS.utils.componentFactory'])
     .controller('socklessComponentCtrl', function ($scope) {
 
     })
     .directive('socklessComponent', function (componentFactory, $log) {
 
         var _linkFn = function link(scope, element, attrs) {
-
             $log.log('sockless component loaded');
         };
 
