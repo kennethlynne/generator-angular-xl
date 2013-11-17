01e1352f8d9f1f7ef8fccc25bf5fb9f3b125a7a0
Kenneth Lynne <kenneth.lynne@gmail.com>
2013-11-17 13:18:06 +0100

    Added customized karma config

--- templates/common/root/karma.conf_preferred.js
+++ templates/common/root/karma.conf_preferred.js
@@ -1,3 +1,75 @@
-/**
- * Created by Kenneth on 17.11.13.
- */
+// Karma configuration
+// http://karma-runner.github.io/0.10/config/configuration-file.html
+
+module.exports = function(config) {
+    config.set({
+        // base path, that will be used to resolve files and exclude
+        basePath: '',
+
+        // testing framework to use (jasmine/mocha/qunit/...)
+        frameworks: ['jasmine'],
+
+        preprocessors: {
+            'app/views/**/*.html': ['ng-html2js'],
+            'app/scripts/**/*.js': ['coverage']
+        },
+
+        // list of files / patterns to load in the browser
+        files: [
+            'app/bower_components/angular/angular.js',
+            'app/bower_components/angular-route/angular-route.js',
+            'app/bower_components/angular-component-factory/angular-component-factory.js',
+            'app/bower_components/angular-mocks/angular-mocks.js',
+            'app/bower_components/angular-route/angular-route.js',
+            'app/bower_components/angular-animate/angular-animate.js',
+            'app/scripts/*.js',
+            'app/scripts/**/*.js',
+            'test/mock/**/*.js',
+            'test/spec/**/*.js',
+            'app/views/**/*.html'
+        ],
+
+        reporters: ['progress', 'coverage'],
+
+        // optionally, configure the reporter
+        coverageReporter: {
+            type : 'html',
+            dir : 'test/coverage/'
+        },
+        // list of files / patterns to exclude
+        exclude: [],
+
+        // web server port
+        port: 8080,
+
+        // level of logging
+        // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+        logLevel: config.LOG_INFO,
+
+
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: true,
+
+
+        // Start these browsers, currently available:
+        // - Chrome
+        // - ChromeCanary
+        // - Firefox
+        // - Opera
+        // - Safari (only Mac)
+        // - PhantomJS
+        // - IE (only Windows)
+        browsers: ['PhantomJS'],
+
+        ngHtml2JsPreprocessor: {
+            // setting this option will create only a single module that contains templates
+            // from all the files, so you can load them all with module('foo')
+            moduleName: 'templates'
+        },
+
+
+        // Continuous Integration mode
+        // if true, it capture browsers, run tests and exit
+        singleRun: false
+    });
+};
