b0b0147e4bb8ebdc58585613de2d445b888e3dc5
Kenneth Lynne <kenneth.lynne@gmail.com>
2013-11-17 19:48:14 +0100

    Awesomified component

--- templates/javascript/component.js
+++ templates/javascript/component.js
@@ -1,19 +1,11 @@
 'use strict';
 
 angular.module('<%= scriptAppName %>')
-    .controller('<%= cameledName %>ComponentCtrl', function ($scope) {
-
+    .controller('<%= cameledName %>ComponentCtrl', function ($scope, $element) {
+        $element.text('this is the <%= _.camelize(name) %> component');
     })
-    .directive('<%= cameledName %>Component', function (componentFactory, $log) {
-
-        var _linkFn = function link(scope, element, attrs) {
-            element.text('this is the <%= _.camelize(name) %> component');
-            $log.log('<%= cameledName %> component loaded');
-        };
-
+    .directive('<%= cameledName %>Component', function (componentFactory) {
         return componentFactory.createComponent('<%= cameledName %>Component', {
-            link: _linkFn,
             controller: '<%= cameledName %>ComponentCtrl'
         });
-        
     });
--- templates/javascript/spec/component.js
+++ templates/javascript/spec/component.js
@@ -2,42 +2,31 @@
 
 describe('Component: <%= cameledName %>Component', function () {
 
-    var element, scope;
+    var element, outerScope, <%= cameledName %>ComponentCtrl, $scope;
 
     beforeEach(function () {
 
         module('<%= scriptAppName %>');
 
-        inject(function ($rootScope) {
-            scope = $rootScope.$new();
+        inject(function ($rootScope, $controller) {
+            $scope = $rootScope.$new();
+            element = angular.element('<<%= _.dasherize(name) %>-component></<%= _.dasherize(name) %>-component>');
+            element = $compile(element)(outerScope);
+            outerScope.$digest();
+
+            <%= cameledName %>ComponentCtrl = $controller('<%= cameledName %>ComponentCtrl', {
+                $scope: $scope,
+                $element: element
+            });
         });
 
     });
 
     it('should have the <%= _.dasherize(name) %>-component class', inject(function ($compile) {
-        element = angular.element('<<%= _.dasherize(name) %>-component></<%= _.dasherize(name) %>-component>');
-        element = $compile(element)(scope);
         expect(element.hasClass('<%= _.dasherize(name) %>-component')).toBeTruthy();
     }));
-});
 
-describe('Component controller: <%= cameledName %>ComponentCtrl', function () {
-
-    var <%= cameledName %>ComponentCtrl, scope;
-
-    beforeEach(function () {
-
-        module('<%= scriptAppName %>');
-
-        inject(function ($controller, $rootScope) {
-            scope = $rootScope.$new();
-            <%= cameledName %>ComponentCtrl = $controller('<%= cameledName %>ComponentCtrl', {
-                $scope: scope
-            });
-        });
-    });
-
-    it('should be tested!', function () {
-        expect(true).toBeTruthy();
+    it('should render a message', function() {
+        expect(element.text()).toContain('this is');
     });
 });
