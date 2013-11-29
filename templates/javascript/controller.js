a96663c45c5c2a546e3a0bc335a41d215377f3c9
Kenneth Lynne <kenneth.lynne@gmail.com>
2013-11-29 15:15:19 +0100

    Supporting promises

--- templates/javascript/controller.js
+++ templates/javascript/controller.js
@@ -4,18 +4,19 @@ angular.module('<%= scriptAppName %>')
     .controller('<%= classedName %>Ctrl', function ($scope, init) {
         $scope.data = init;
     })
-    .service('<%= classedName %>CtrlInit', function ($q) {
+    .service('<%= classedName %>CtrlInit', function ($q, $log) {
 
         var _prepare = function () {
-            var deferred = $q.defer();
+            $log.log("<%= classedName %>Ctrl loading");
 
-            deferred.resolve([
-                'Awe',
-                'Some',
-                'Sauce'
-            ]);
+            return $q.all(['Data from service 1', 'Data from service 2']).then(function (data) {
+                $log.log("<%= classedName %>Ctrl loaded!");
 
-            return deferred.promise;
+                return {
+                    message1: data[0],
+                    message2: data[1]
+                }
+            });
         };
 
         return {
