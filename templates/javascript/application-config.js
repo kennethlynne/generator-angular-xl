b922fb001b519335cd1a314cb1ba3f933070960d
Kenneth Lynne <kenneth.lynne@gmail.com>
2013-12-07 19:52:51 +0100

    Config spinner

--- templates/javascript/application-config.js
+++ templates/javascript/application-config.js
@@ -8,5 +8,6 @@ angular.module('<%= scriptAppName %>').constant('Config', {
         path:               '/api',
         fakeDelay:          2000
     }
-});
+})
+.value('cgBusyTemplateName','views/angular-busy/default-spinner.html');
 
