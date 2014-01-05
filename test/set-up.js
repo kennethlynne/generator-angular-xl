e31989346fd5cf4a0ca29f439657e15c7ce90d3a
Kenneth Lynne <kenneth.lynne@gmail.com>
2014-01-05 03:19:31 +0100

    run generated codes tests

---
+++ test/set-up.js
@@ -0,0 +1,42 @@
+'use strict';
+var fs = require('fs');
+var path = require('path');
+var util = require('util');
+var helpers = require('yeoman-generator').test;
+var _ = require('underscore.string');
+
+var deps = [
+    '../../app',
+    '../../common',
+    '../../controller',
+    '../../view',
+    '../../page',
+    '../../component',
+    '../../service',
+    '../../main', [
+        helpers.createDummyGenerator(),
+        'karma:app'
+    ]
+];
+
+helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
+    var angular = helpers.createGenerator('angular-xl:app', deps);
+    var pageGenerator = helpers.createGenerator('angular-xl:page', deps, ['thingThing']);
+    var componentGenerator = helpers.createGenerator('angular-xl:component', deps, ['thingThing']);
+    var serviceGenerator = helpers.createGenerator('angular-xl:service', deps, ['serviceThing']);
+
+    angular.options['skip-install'] = true;
+
+    helpers.mockPrompt(angular, {
+        modules: []
+    });
+
+    angular.run([], function () {
+        pageGenerator.run([], function () {
+            componentGenerator.run([], function () {
+                serviceGenerator.run([], function () {}
+                );}
+            );
+        });
+    });
+});
--- tests.sh
+++ tests.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 cd test/temp
+rm -rf .
+node ../set-up.js
 npm install
 bower install
 npm install -g grunt-cli
