d9ed08b9e8606734502fa38ca63aeb9074499dd5
Kenneth Lynne <kenneth.lynne@gmail.com>
2014-01-08 20:11:31 +0100

    fix paths

--- app/index.js
+++ app/index.js
@@ -133,14 +133,14 @@ Generator.prototype.packageFiles = function () {
     this.template('../../templates/javascript/framework/config.js', 'app/scripts/config/config.js');
     this.template('../../templates/javascript/framework/errorCtrl.js', 'app/pages/error/index/error.js');
     this.template('../../templates/javascript/framework/regex-escape.js', 'app/scripts/framework/regex-escape.js');
-    this.template('../../templates/javascript/spec/errorCtrl.js', 'test/spec/unit/pages/error/index/error.js');
+    this.template('../../templates/javascript/spec/errorCtrl.js', 'test/unit/spec/pages/error/index/error.js');
     this.template('../../templates/javascript/framework/mainCtrl.js', 'app/pages/index/index/index.js');
-    this.template('../../templates/javascript/spec/mainCtrl.js', 'test/spec/unit/pages/index/index/index.js');
+    this.template('../../templates/javascript/spec/mainCtrl.js', 'test/unit/spec/pages/index/index/index.js');
     this.template('../../templates/javascript/framework/mock-api.js', 'app/scripts/mock-api.js');
     this.template('../../templates/javascript/framework/page-view-directory-factory.js', 'app/scripts/framework/page-view-directory-factory.js');
     this.template('../../templates/javascript/framework/state-factory.js', 'app/scripts/framework/state-factory.js');
     this.template('../../templates/javascript/spec/page-view-directory-factory.js', 'test/spec/unit/framework/page-view-directory-factory.js');
-    this.template('../../templates/javascript/spec/state-factory.js', 'test/spec/unit/framework/state-factory.js');
+    this.template('../../templates/javascript/spec/state-factory.js', 'test/unit/spec/framework/state-factory.js');
     this.template('../../templates/javascript/framework/dasherize.js', 'app/scripts/framework/dasherize.js');
-    this.template('../../templates/javascript/spec/dasherize.js', 'test/spec/unit/framework/dasherize.js');
+    this.template('../../templates/javascript/spec/dasherize.js', 'test/unit/spec/framework/dasherize.js');
 };
