b4aaf897061bdfa78002cae2e41878a74fbdbbf7
Kenneth Lynne <kenneth.lynne@gmail.com>
2014-01-10 21:52:47 +0100

    add testable stuff

--- templates/javascript/navbar.js
+++ templates/javascript/navbar.js
@@ -2,6 +2,7 @@
 
 angular.module('<%= scriptAppName %>.components')
     .controller('navbarComponentCtrl', function ($scope, $element) {
+        $scope.text = 'this is the navbar component';
     })
     .component('navbar', function () {
         return {
