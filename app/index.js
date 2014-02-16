a26da379ac7943e59c94f3f00ca4c7e1fcf63bc0
Kenneth Lynne <kenneth.lynne@gmail.com>
2014-02-16 22:07:26 +0100

    move api base url to config

--- app/index.js
+++ app/index.js
@@ -148,7 +148,6 @@ Generator.prototype.packageFiles = function () {
     this.template('../../templates/common/_bower.json', 'bower.json');
     this.template('../../templates/common/_package.json', 'package.json');
     this.template('../../templates/common/Gruntfile.js', 'Gruntfile.js');
-    this.template('../../templates/javascript/framework/api-base-url.js', 'app/scripts/framework/api-base-url.js');
     this.template('../../templates/javascript/framework/config.js', 'app/config/config.js');
     this.template('../../templates/javascript/framework/errorCtrl.js', 'app/pages/error/index/error.js');
     this.template('../../templates/javascript/navbar.js', 'app/components/navbar/navbar.js');
--- templates/javascript/framework/config.js
+++ templates/javascript/framework/config.js
@@ -15,7 +15,13 @@
         .config(function (componentFactoryProvider) { componentFactoryProvider.setViewPath(function (componentSnakeName, componentName) {
             return 'components/' + componentSnakeName + '/' + componentSnakeName + '.html';
         })})
-        .value('cgBusyTemplateName','views/angular-busy/default-spinner.html')<% if (xeditableModule) { %>
+        .value('cgBusyTemplateName','views/angular-busy/default-spinner.html')
+        .factory('BaseUrl', function (Config) {
+            return (Config.API.protocol + '://' + Config.API.host + ':' + Config.API.port + '/');
+        })
+        .factory('APIBaseUrl', function (Config) {
+            return (Config.API.protocol + '://' + Config.API.host + ':' + Config.API.port + Config.API.path + '/');
+        })<% if (xeditableModule) { %>
         .run(function(editableOptions) {editableOptions.theme = 'bs3'})<% } %>;
 
 
