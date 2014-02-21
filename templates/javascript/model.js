84107078fd8c69a31ce21ffe5f1d64e663758c28
Kenneth Lynne <kenneth.lynne@gmail.com>
2014-02-21 02:53:40 +0100

    improve readability

--- templates/javascript/model.js
+++ templates/javascript/model.js
@@ -1,28 +1,30 @@
 'use strict';
 
 angular.module('<%= scriptAppName %>')
-    .factory('<%= classedName %>Model', function ($q, $http, $rootScope, BaseModel, APIBaseUrl, <%= classedName %>Repository) {
+    .factory('Model', function ($q, $http, $rootScope, BaseModel, APIBaseUrl, $injector) {
+
+        var Repository = $injector.get('<%= classedName %>Repository');
 
         var url = APIBaseUrl + '<%= pluralizedName %>';
 
-        function <%= classedName %>Model(data) {
+        function Model(data) {
             data = data || {};
             data.url = url;
             BaseModel.call(this,data);
-        };
+        }
 
-        <%= classedName %>Model.$settings = {url:url};
-        <%= classedName %>Model.prototype = Object.create(BaseModel.prototype);
+        Model.$settings = {url:url};
+        Model.prototype = Object.create(BaseModel.prototype);
 
         //Decorate save to attach this item to the Repository on successful save
-        var _$save = <%= classedName %>Model.prototype.$save;
-        <%= classedName %>Model.prototype.$save = function () {
-            var <%= classedName %> = this;
+        var _$save = Model.prototype.$save;
+        Model.prototype.$save = function () {
+            var self = this;
             return _$save.apply(this, arguments).then(function (response) {
-                <%= classedName %>Repository.attach(<%= classedName %>);
+                Repository.attach(self);
                 return response;
             });
         };
 
-        return <%= classedName %>Model;
+        return Model;
     });
