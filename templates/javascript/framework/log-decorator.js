ec8d395e001d1d9161146645e0afab7bc3e1237f
Kenneth Lynne <kenneth.lynne@gmail.com>
2014-04-08 22:12:31 +0200

    jshint

--- templates/javascript/framework/log-decorator.js
+++ templates/javascript/framework/log-decorator.js
@@ -10,10 +10,10 @@ angular.module('<%= scriptAppName %>')
       };
 
       $delegate.debug = function () {
-        if (Config.environment == 'development') _log.apply(null, arguments);
+        if (Config.environment === 'development') _log.apply(null, arguments);
       };
 
-      if (Config.environment == 'development') $delegate.log('[c="font-family: \'Helvetica Neue\', Helvetica, Arial, sans-serif; color: #fff; font-size: 20px; padding: 15px 20px; background: #444; border-radius: 4px; line-height: 100px; text-shadow: 0 1px #000"]development mode[c]');
+      if (Config.environment === 'development') $delegate.log('[c="font-family: \'Helvetica Neue\', Helvetica, Arial, sans-serif; color: #fff; font-size: 20px; padding: 15px 20px; background: #444; border-radius: 4px; line-height: 100px; text-shadow: 0 1px #000"]development mode[c]');
 
       return $delegate;
 
