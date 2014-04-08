d57272ff5f9b86cdccc620ce064a8531e3b43757
Kenneth Lynne <kenneth.lynne@gmail.com>
2014-04-09 01:21:21 +0200

    clean up

--- templates/javascript/spec/controller.js
+++ templates/javascript/spec/controller.js
@@ -20,27 +20,3 @@ describe('Controller(<%= stateUrl %>): <%= classedName %>Ctrl', function () {
     expect(scope.foo).toEqual('bar');
   });
 });
-<% if (initService) { %>
-describe('Service(<%= stateUrl %>): <%= classedName %>CtrlInit', function () {
-
-  var <%= classedName %>CtrlInit;
-
-  beforeEach(function () {
-
-    module('<%= scriptAppName %>');
-
-    inject(function (_<%= classedName %>CtrlInit_) {
-      <%= classedName %>CtrlInit = _<%= classedName %>CtrlInit_;
-    });
-
-  });
-
-  it('should have a prepare function', function () {
-    expect(typeof <%= classedName %>CtrlInit.prepare).toEqual('function');
-  });
-
-  it('should return a promise', function () {
-    expect(typeof <%= classedName %>CtrlInit.prepare().then).toEqual('function');
-  });
-
-});<% } %>
