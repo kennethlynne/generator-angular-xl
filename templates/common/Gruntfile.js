ecd9aa0dfc50fbb5c61f27617091864eec63439a
Kenneth Lynne <kenneth.lynne@gmail.com>
2014-04-08 19:41:23 +0200

    use appJs in jshint cfg

--- templates/common/Gruntfile.js
+++ templates/common/Gruntfile.js
@@ -122,10 +122,7 @@ module.exports = function (grunt) {
                 jshintrc: '.jshintrc',
                 reporter: require('jshint-stylish')
             },
-            all: [
-                'Gruntfile.js',
-                '<%%= yeoman.app %>/scripts/**/*.js'
-            ]
+            all: appJs
         },
         coffee: {
             options: {
