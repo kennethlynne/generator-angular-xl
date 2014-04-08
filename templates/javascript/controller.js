4f3264687a39a8aaa13fbaeb4cea5c321c455e35
Kenneth Lynne <kenneth.lynne@gmail.com>
2014-04-08 22:27:07 +0200

    clean up controller template

--- templates/javascript/controller.js
+++ templates/javascript/controller.js
@@ -6,33 +6,7 @@ angular.module('<%= scriptAppName %>')
       url: '<%= stateUrl %>',
       templateUrl: '<%= viewTemplateUrl %>'
     }));
-  })<% if (initService) { %>
-  .service('<%= classedName %>CtrlInit', function ($q, $log) {
-
-    /**
-    * An array of functions that return either a value or a promise.
-    * For example:
-    *      UserReposiitory.getAll()
-    *      'Hello world'
-    */
-    var dependancies = [],
-
-    /**
-    * Callback that is called when all promises and values are resolved.
-    * It is called with an array with the resolved values corresponding to the dependancy array.
-    * The data returned from this function is injected into the controller as 'init'
-    */
-    finishedCb = function (reponse) {
-      $log.debug('<%= classedName %> loaded. Data:', response);
-      return {};
-    };
-
-    return {
-      prepare: function () {
-        $log.debug('<%= classedName %> loading');
-        return $q.all(dependancies).then(finishedCb);
-      }
-    }
-  })< % } %>.controller('<%= classedName %>Ctrl', function ($scope < % if (initService) { %>, init < % } %>) {
+  })
+  .controller('<%= classedName %>Ctrl', function ($scope) {
     $scope.foo = 'bar';
   });
